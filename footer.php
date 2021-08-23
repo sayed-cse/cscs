33c9b2cf1499f372b7d887537b907d0877172a60
FireFly <78496742+sayed-cse@users.noreply.github.com>
2021-08-24 04:53:34 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,7 +13,6 @@
 		$("#slideshow > div:gt(0)").hide();setInterval(function(){
 			$('#slideshow > div:first')  .fadeOut(1000)  .next()  .fadeIn(1000)  .end()  .appendTo('#slideshow');
 		},3000);
-
 	});
 /*InputField Reset*/	
 	function clearText(field){field.defaultValue == field.value ? field.value = '' : field.value = field.defaultValue;}
